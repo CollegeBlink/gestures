5c025451054f21397ba54932627089e04e3d2dc6
dumesm1 <markdumes@Marks-MacBook-Air.local>
2014-07-01 19:10:47 -0500

    Gestures
    This projects covers how to add gestures to a project

--- gestures/ViewController.h
+++ gestures/ViewController.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+- (IBAction)foundtap:(id)sender;
+- (IBAction)foundpinch:(id)sender;
+- (IBAction)foundrotation:(id)sender;
+- (IBAction)foundswipe:(id)sender;
+
 
 @end
--- gestures/ViewController.m
+++ gestures/ViewController.m
@@ -7,13 +7,17 @@
 //
 
 #import "ViewController.h"
-
+#define kOriginWidth 240.0
+#define kOriginHeight 128.0
+#define kOriginX 40.0
+#define kOriginY 427.0
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
-
+@synthesize image;
+@synthesize label;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -26,4 +30,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)foundtap:(id)sender {
+    [label setText:@"Tapped"];
+}
+- (IBAction)foundpinch:(id)sender {
+    
+    UIPinchGestureRecognizer *recognizer;
+    NSString *feedback;
+    double scale;
+    
+        recognizer=(UIPinchGestureRecognizer *)sender;
+    scale = recognizer.scale;
+    image.transform = CGAffineTransformMakeRotation(0.0);
+        feedback=[[NSString alloc]
+                     initWithFormat:@"Pinched, Scale:%1.2f,Velocity:%1.2f",recognizer.scale,recognizer.velocity];
+    label.text=feedback;
+    image.frame=CGRectMake(kOriginX,
+            kOriginY,kOriginWidth*scale,kOriginHeight*scale);
+}
+
+- (IBAction)foundrotation:(id)sender {
+    UIRotationGestureRecognizer *recognizer;
+    NSString *feedback;
+    double rotation;
+    
+recognizer=(UIRotationGestureRecognizer *)sender;
+rotation=recognizer.rotation;
+feedback=[[NSString alloc]initWithFormat:@"Rotated, Radians:%1.2f, Velocity:%1.2f",recognizer.rotation, recognizer.velocity];
+    label.text=feedback;
+    image.transform = CGAffineTransformMakeRotation(rotation);
+}
+- (IBAction)foundswipe:(id)sender {
+    [label setText:@"Swiped"];
+}
 @end
